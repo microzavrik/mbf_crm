69ce9f16941318eeced32bd2fe18181b03184c77
zsdasdsad <au30286@gmail.com>
2024-06-27 04:02:06 +0500

    ну кароч хуйня тут ну хз кароч

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<helloPage :test="test" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
+
+<script>
+import auth from "./components/auth.vue";
+import helloPage from './components/helloPage.vue'
+
+export default{
+  components:{ auth, helloPage },
+  data(){
+    return{
+    }
+    },
+  methods:{
+    test(){
+        console.log(1)
+    }
+  }
+}
+</script>
